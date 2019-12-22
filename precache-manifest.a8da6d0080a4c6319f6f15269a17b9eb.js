16715129b13f348c2e0cc1e96fcbbbc706e7e328
Artur <nikitsin.artur@gmail.com>
2019-12-22 11:58:54 +0300

    Updates

---
+++ precache-manifest.a8da6d0080a4c6319f6f15269a17b9eb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2971b2be98be5dfd98fc3dff0d4ece9f",
+    "url": "berdmival.github.io/derevenskoe/index.html"
+  },
+  {
+    "revision": "9b904550d48c95c1adba",
+    "url": "berdmival.github.io/derevenskoe/static/css/2.4c487673.chunk.css"
+  },
+  {
+    "revision": "bfe8c87be0141d555dc3",
+    "url": "berdmival.github.io/derevenskoe/static/css/main.f92cd3a7.chunk.css"
+  },
+  {
+    "revision": "9b904550d48c95c1adba",
+    "url": "berdmival.github.io/derevenskoe/static/js/2.2bdec7a2.chunk.js"
+  },
+  {
+    "revision": "bfe8c87be0141d555dc3",
+    "url": "berdmival.github.io/derevenskoe/static/js/main.9ea05754.chunk.js"
+  },
+  {
+    "revision": "48e80ad396d9d859417e",
+    "url": "berdmival.github.io/derevenskoe/static/js/runtime-main.165734ec.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/derevenskoe/precache-manifest.dae2b9f9b5d2785f8094b1f8a67f667f.js"
+  "berdmival.github.io/derevenskoe/precache-manifest.a8da6d0080a4c6319f6f15269a17b9eb.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/derevenskoe/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("berdmival.github.io/derevenskoe/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
