ffdb5129cc5cb6e8a3895033508e230f9c1b2eb2
Artur <nikitsin.artur@gmail.com>
2019-12-22 11:57:28 +0300

    Updates

---
+++ precache-manifest.dae2b9f9b5d2785f8094b1f8a67f667f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a86ba62bf3901f4d123cf4136d77699f",
+    "url": "/derevenskoe/index.html"
+  },
+  {
+    "revision": "9b904550d48c95c1adba",
+    "url": "/derevenskoe/static/css/2.4c487673.chunk.css"
+  },
+  {
+    "revision": "bfe8c87be0141d555dc3",
+    "url": "/derevenskoe/static/css/main.f92cd3a7.chunk.css"
+  },
+  {
+    "revision": "9b904550d48c95c1adba",
+    "url": "/derevenskoe/static/js/2.2bdec7a2.chunk.js"
+  },
+  {
+    "revision": "bfe8c87be0141d555dc3",
+    "url": "/derevenskoe/static/js/main.9ea05754.chunk.js"
+  },
+  {
+    "revision": "1b86c8048e7247b2840b",
+    "url": "/derevenskoe/static/js/runtime-main.5273cf60.js"
+  }
+]);
