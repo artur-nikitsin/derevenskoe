32f9310a3618c8713f7ba90cc848050c14ce4bb7
Artur <nikitsin.artur@gmail.com>
2019-12-22 11:38:22 +0300

    Updates

---
+++ precache-manifest.b25d3eb82dbdaf5f4691db904a2d70c1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "93c357198fc090b7d709539fea1b0600",
+    "url": "berdmival.github.io/derevenskoe/index.html"
+  },
+  {
+    "revision": "9b904550d48c95c1adba",
+    "url": "berdmival.github.io/derevenskoe/static/css/2.4c487673.chunk.css"
+  },
+  {
+    "revision": "162829248475ca5e73c1",
+    "url": "berdmival.github.io/derevenskoe/static/css/main.f92cd3a7.chunk.css"
+  },
+  {
+    "revision": "9b904550d48c95c1adba",
+    "url": "berdmival.github.io/derevenskoe/static/js/2.2bdec7a2.chunk.js"
+  },
+  {
+    "revision": "162829248475ca5e73c1",
+    "url": "berdmival.github.io/derevenskoe/static/js/main.33213a7b.chunk.js"
+  },
+  {
+    "revision": "48e80ad396d9d859417e",
+    "url": "berdmival.github.io/derevenskoe/static/js/runtime-main.165734ec.js"
+  }
+]);
