a1954243c8bd926f5151deee4e7f648b71b8e9a6
berdmival <mtsby7049874@gmail.com>
2019-12-28 01:27:09 +0300

    temporary off security

--- src/main/java/by/berdmival/derevenskoe/controller/ClientController.java
+++ src/main/java/by/berdmival/derevenskoe/controller/ClientController.java
@@ -1,24 +1,24 @@
-package by.berdmival.derevenskoe.controller;
-
-import by.berdmival.derevenskoe.service.security.ClientService;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
-import org.springframework.security.oauth2.provider.ClientDetails;
-import org.springframework.security.oauth2.provider.client.BaseClientDetails;
-import org.springframework.web.bind.annotation.*;
-
-@RestController
-public class ClientController {
-    @Autowired
-    private ClientService clientService;
-
-    @GetMapping(path = "/clients/{clientId}")
-    public ResponseEntity<ClientDetails> getClientById(@PathVariable("clientId") String clientId) {
-        return ResponseEntity.ok(clientService.getClientById(clientId));
-    }
-
-    @PostMapping(path = "/clients")
-    public ResponseEntity<ClientDetails> addClient(@RequestBody BaseClientDetails clientDetails) {
-        return ResponseEntity.ok(clientService.addClient(clientDetails));
-    }
-}
+//package by.berdmival.derevenskoe.controller;
+//
+//import by.berdmival.derevenskoe.service.security.ClientService;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.http.ResponseEntity;
+//import org.springframework.security.oauth2.provider.ClientDetails;
+//import org.springframework.security.oauth2.provider.client.BaseClientDetails;
+//import org.springframework.web.bind.annotation.*;
+//
+//@RestController
+//public class ClientController {
+//    @Autowired
+//    private ClientService clientService;
+//
+//    @GetMapping(path = "/clients/{clientId}")
+//    public ResponseEntity<ClientDetails> getClientById(@PathVariable("clientId") String clientId) {
+//        return ResponseEntity.ok(clientService.getClientById(clientId));
+//    }
+//
+//    @PostMapping(path = "/clients")
+//    public ResponseEntity<ClientDetails> addClient(@RequestBody BaseClientDetails clientDetails) {
+//        return ResponseEntity.ok(clientService.addClient(clientDetails));
+//    }
+//}
--- src/main/java/by/berdmival/derevenskoe/controller/UserController.java
+++ src/main/java/by/berdmival/derevenskoe/controller/UserController.java
@@ -1,36 +1,36 @@
-package by.berdmival.derevenskoe.controller;
-
-import by.berdmival.derevenskoe.entity.account.Account;
-import by.berdmival.derevenskoe.service.account.AccountService;
-import by.berdmival.derevenskoe.service.security.UserService;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
-
-import java.security.Principal;
-
-@RestController
-public class UserController {
-    @Autowired
-    private UserService userService;
-    @Autowired
-    private AccountService accountService;
-
-    @PostMapping(path = "/users")
-    public ResponseEntity<UserDetails> addUser(@RequestParam String username, @RequestParam String password) {
-        UserDetails userDetails = userService.addUser(username, password);
-        Account account = new Account();
-        account.setUsername(userDetails.getUsername());
-        accountService.add(account);
-        return ResponseEntity.ok(userDetails);
-    }
-
-    @RequestMapping("/users/validate")
-    public Principal user(Principal user) {
-        return user;
-    }
-}
+//package by.berdmival.derevenskoe.controller;
+//
+//import by.berdmival.derevenskoe.entity.account.Account;
+//import by.berdmival.derevenskoe.service.account.AccountService;
+//import by.berdmival.derevenskoe.service.security.UserService;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.http.ResponseEntity;
+//import org.springframework.security.core.userdetails.UserDetails;
+//import org.springframework.web.bind.annotation.PostMapping;
+//import org.springframework.web.bind.annotation.RequestMapping;
+//import org.springframework.web.bind.annotation.RequestParam;
+//import org.springframework.web.bind.annotation.RestController;
+//
+//import java.security.Principal;
+//
+//@RestController
+//public class UserController {
+//    @Autowired
+//    private UserService userService;
+//    @Autowired
+//    private AccountService accountService;
+//
+//    @PostMapping(path = "/users")
+//    public ResponseEntity<UserDetails> addUser(@RequestParam String username, @RequestParam String password) {
+//        UserDetails userDetails = userService.addUser(username, password);
+//        Account account = new Account();
+//        account.setUsername(userDetails.getUsername());
+//        accountService.add(account);
+//        return ResponseEntity.ok(userDetails);
+//    }
+//
+//    @RequestMapping("/users/validate")
+//    public Principal user(Principal user) {
+//        return user;
+//    }
+//}
