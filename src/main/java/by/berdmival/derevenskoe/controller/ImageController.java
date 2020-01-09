7f4cd85421ada5472083947dbe5de78de78d76e6
berdmival <mtsby7049874@gmail.com>
2020-01-09 14:43:06 +0300

    showing different types of images from server

--- src/main/java/by/berdmival/derevenskoe/controller/ImageController.java
+++ src/main/java/by/berdmival/derevenskoe/controller/ImageController.java
@@ -1,12 +1,12 @@
 package by.berdmival.derevenskoe.controller;
 
+import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
 
 import java.io.File;
@@ -19,6 +19,7 @@ public class ImageController {
     @Value("${upload.path}")
     private String uploadPath;
 
+    @ApiOperation(value = "Testing of loading images from server")
     @GetMapping("/testimg/{id}/{img}")
     public ModelAndView testImage(ModelAndView modelAndView, @PathVariable String img, @PathVariable String id) {
         modelAndView.addObject("id", id);
@@ -27,12 +28,9 @@ public class ImageController {
         return modelAndView;
     }
 
-    @GetMapping(
-            value = "/img/{id}/{img}",
-            produces = MediaType.IMAGE_JPEG_VALUE
-    )
-    public @ResponseBody
-    byte[] getImageWithMediaType(@PathVariable String id, @PathVariable String img) throws IOException {
-        return Files.readAllBytes(new File(uploadPath + "/" + id + "/" + img).toPath());
+    @ApiOperation(value = "Load images from server")
+    @GetMapping("/img/{id}/{img}")
+    public ResponseEntity<byte[]> getImageWithMediaType(@PathVariable String id, @PathVariable String img) throws IOException {
+        return ResponseEntity.ok(Files.readAllBytes(new File(uploadPath + "/" + id + "/" + img).toPath()));
     }
 }
