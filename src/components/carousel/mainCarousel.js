82fe8b65877b7c8c80f8a824847faf4b6577931e
Nikitsin Artur <nikitsin.artur@gmail.com>
2020-01-10 22:23:37 +0300

    feature: add new setiings to caroucel

--- src/components/carousel/mainCarousel.js
+++ src/components/carousel/mainCarousel.js
@@ -3,10 +3,13 @@ import Slider from "react-slick";
 import {Container} from '@material-ui/core';
 import ArrowBackIosIcon from '@material-ui/icons/ArrowBackIos';
 import ArrowForwardIosIcon from '@material-ui/icons/ArrowForwardIos';
+import Grid from '@material-ui/core/Grid';
 import "./styles/carousel.scss"
 
 class MainCarousel extends Component {
+
     render() {
+
 
         function NextArrow(props) {
             const {className, onClick} = props;
@@ -15,7 +18,7 @@ class MainCarousel extends Component {
                 <ArrowForwardIosIcon
                     className={className}
                     onClick={onClick}
-                    style={{fontSize: 100}}/>
+                    style={{fontSize: 50}}/>
             );
         }
 
@@ -25,7 +28,7 @@ class MainCarousel extends Component {
                 <ArrowBackIosIcon
                     className={className}
                     onClick={onClick}
-                    style={{fontSize: 100}}/>
+                    style={{fontSize: 50}}/>
 
             );
         }
@@ -35,23 +38,57 @@ class MainCarousel extends Component {
             dots: true,
             infinite: true,
             speed: 500,
-            slidesToShow: 1,
-            slidesToScroll: 1,
-            centerMode: true,
-            centerPadding: 200,
+            adaptiveHeight: true,
             arrows: true,
+            useCSS: true,
+            responsive: [
+                {
+                    breakpoint: 1920,
+                    settings: {
+                        slidesToShow: 2,
+                        slidesToScroll: 2,
+                        adaptiveHeight: true,
+                        infinite: true,
+                        dots: true
+                    }
+                },
+                {
+                    breakpoint: 600,
+                    settings: {
+                        slidesToShow: 1,
+                        slidesToScroll: 1,
+                        initialSlide: 1,
+                        adaptiveHeight: true,
+                    }
+                },
+                {
+                    breakpoint: 480,
+                    settings: {
+                        slidesToShow: 1,
+                        slidesToScroll: 1,
+                        adaptiveHeight: true,
+                    }
+                }
+            ],
+
             nextArrow: <NextArrow/>,
             prevArrow: <PrevArrow/>
 
         };
 
-       /*TODO:    realize images requiring from components folders  */
+        /*TODO:    realize images requiring from components folders  */
 
         return (
             <Container className={"container"}>
                 <Slider {...settings}>
                     <div>
                         <img src="assets/caroucel/10022a-960x400.webp" alt=""/>
+                    </div>
+                    <div>
+                        <img src="assets/caroucel/bbab61-960x400.webp" alt=""/>
+                    </div>
+                    <div>
+                        <img src="assets/caroucel/bbab61-960x400.webp" alt=""/>
                     </div>
                     <div>
                         <img src="assets/caroucel/bbab61-960x400.webp" alt=""/>
